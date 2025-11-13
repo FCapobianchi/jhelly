a11ba0116b44adf0a726c68a8120505c7df087df
FCapobianchi <fabiano@juicenet.it>
2025-11-13 16:34:37 +0100

    Visualizzazione devices

--- app/Presentation/Home/HomePresenter.php
+++ app/Presentation/Home/HomePresenter.php
@@ -4,14 +4,30 @@ declare(strict_types=1);
 
 namespace App\Presentation\Home;
 
-use App\Core\RequireLoggedUser;
 use Nette;
-
+use App\Core\RequireLoggedUser;
+use App\Model\DeviceFacade;
+use Nette\Utils\ArrayHash;
+use Nette\Database\Table\ActiveRow;
 
 final class HomePresenter extends Nette\Application\UI\Presenter {
     use RequireLoggedUser;
     
-    public function renderDefault($id):void {
+    public function __construct(private DeviceFacade $modelDevice) {
+    }
 
+    public function renderDefault($id):void {
+        $selection = $this->modelDevice->getDevices();
+        foreach($selection as $record){
+            $array = $record->toArray();
+            $array['info'] = unserialize($array['info']);
+            $array['attributes'] = unserialize($array['attributes']);
+            $device = ArrayHash::from($array);
+            $protocol = ($array['port']==80)?'http':'https';
+            //$auth = ($device->attributes->auth)?"{$device->user}:{$device->password}@":'';
+            //$device->base_uri = "{$protocol}://{$auth}{$device->host}:{$device->port}";
+            $device->base_uri = "{$protocol}://{$device->host}:{$device->port}";
+            $this->template->devices[]=$device;
+        }
     }
 }
